b74a4317169a2b4728470baa5ebd3d0b3e199d11
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-02-12 11:00:27 +0100

    Extract pytest-slc from pytest-plugins repository.

--- noxconfig.py
+++ noxconfig.py
@@ -5,5 +5,5 @@ from exasol.toolbox.config import BaseConfig
 PROJECT_CONFIG = BaseConfig(
     root_path=Path(__file__).parent,
     project_name="pytest_slc",
-    python_versions=("3.10", "3.11", "3.12", "3.13"),
+    python_versions=("3.10", "3.11", "3.12", "3.13", "3.14"),
 )
