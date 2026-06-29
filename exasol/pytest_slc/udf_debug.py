a55568d876a6f2a71a33899c20278e0835b5d911
ckunki <christoph.kuhnke@exasol.com>
2026-06-29 13:17:26 +0200

    Fixed type errors

--- exasol/pytest_slc/udf_debug.py
+++ exasol/pytest_slc/udf_debug.py
@@ -1,4 +1,6 @@
-"""Support for capturing the output of UDFs."""
+"""
+Support for capturing the output of UDFs.
+"""
 
 import io
 import socket
@@ -7,16 +9,27 @@ import sys
 import threading
 import time
 import traceback
-from multiprocessing import Process, Queue
+from multiprocessing import (
+    Process,
+    Queue,
+)
 from queue import Full
 from threading import Thread
-from typing import Optional, Tuple
+from typing import (
+    Callable,
+    TextIO,
+    TypeAlias,
+)
 
-from exasol_python_test_framework import docker_db_environment, udf
+
+TextStream: TypeAlias = TextIO | io.TextIOBase
+QueryExecutor: TypeAlias = Callable[[str], None]
 
 
 class LogHandler(socketserver.StreamRequestHandler):
-    """Read output lines from the UDF log socket."""
+    """
+    Read output lines from the UDF log socket.
+    """
 
     def handle(self):
         address = f"{self.client_address[0]}:{self.client_address[1]}"
@@ -31,26 +44,31 @@ class LogHandler(socketserver.StreamRequestHandler):
                 try:
                     self.server.output.put_nowait(message)
                 except Full as full_ex:
+                    # Should that be log.error() ?
                     print(f"Queue is full for UDF debug:{full_ex}", file=sys.stderr)
                 buffer = []
 
 
 class LogServer(socketserver.ThreadingTCPServer):
-    """Background socket server that forwards messages into a queue."""
+    """
+    Background socket server that forwards messages into a queue.
+    """
 
     allow_reuse_address = True
     daemon_threads = True
 
-    def __init__(self, server_address: Tuple[str, int], output: Queue):
+    def __init__(self, server_address: tuple[str, int], output: Queue):
         output.put_nowait(f"Server address:{server_address}\n")
         self.output = output
         super().__init__(server_address, LogHandler)
 
 
 class ScriptOutputThread(Thread):
-    """Serve UDF output in a background thread."""
+    """
+    Serve UDF output in a background thread.
+    """
 
-    def __init__(self, server_address: Tuple[str, int], output: Queue):
+    def __init__(self, server_address: tuple[str, int], output: Queue):
         super().__init__()
         self.server_address = server_address
         self.server = LogServer(server_address, output)
@@ -65,48 +83,72 @@ class ScriptOutputThread(Thread):
             del self.server
 
 
-def output_service(queue: Queue, server: Optional[str], port: Optional[int]):
-    """Start a standalone output service.
-
-    This service can be used in another Python or R instance, for
-    Python instances the connection parameter externalClient need to
-    be specified.
-    """
+def default_host() -> str:
     try:
-        host = socket.gethostbyname(socket.gethostname())
+        return socket.gethostbyname(socket.gethostname())
     except OSError:
-        host = "0.0.0.0"
+        return "0.0.0.0"
 
-    if server is None:
-        server = host
-    if port is None:
-        port = 3000
 
-    address = server, port
+# Is some renaming acceptable, here?
+# - rename argument "server" to "host"
+# - rename the function to start_output_service()
+def output_service(queue: Queue, server: str | None, port: int | None):
+    """
+    Start a standalone output service.
 
-    server = ScriptOutputThread(server_address=address, output=queue)
-    queue.put_nowait(f">>> bind the output server to {server}:{port}")
+    This service can be used in another Python or R instance, for Python
+    instances the connection parameter externalClient need to be specified.
+    """
+
+    host = default_host() if server is None else server
+    port = 3000 if port is None else port
+    thread = ScriptOutputThread(server_address=(host, port), output=queue)
+    queue.put_nowait(f">>> bind the output to {host}:{port}")
     try:
-        server.run()
+        thread.run()
     except KeyboardInterrupt:
         sys.stdout.flush()
 
 
+class Consumer(Thread):
+    def __init__(self, queue: Queue, output: TextStream):
+        super().__init__(target=self.print)
+        self._queue = queue
+        self._continue = True
+        self._output = output
+
+    def stop(self) -> None:
+        self._continue = False
+
+    def print(self):
+        while self._continue:
+            try:
+                message = self._queue.get()
+                self._output.write(f"UDF DEBUG {message}\n")
+            except (OSError, ValueError):
+                traceback.print_exc()
+        self._queue.close()
+
+
 def start_udf_output_redirect_consumer(
-    test_case: udf.TestCase, server: Optional[str], output: io.TextIOBase
+    query: QueryExecutor, server: str | None, output: io.TextIOBase
+    # test_case: udf.TestCase, server: Optional[str], output: io.TextIOBase
 ):
-    """Start the output forwarding process and its consumer thread."""
+    """
+    Start the output forwarding process and its consumer thread.
+    """
 
     if server is None:
         hostname = socket.gethostname()
         local_ip = socket.gethostbyname(hostname)
     else:
         local_ip = server
-    print("local_ip", local_ip)
+    print("local_ip", local_ip) # should this be log.debug() ?
 
     port = 3000
 
-    queue = Queue()
+    queue: Queue = Queue()
     process = Process(target=output_service, args=(queue, local_ip, port))
     process.start()
 
@@ -120,29 +162,34 @@ def start_udf_output_redirect_consumer(
                 traceback.print_exc()
         queue.close()
 
-    stdout_thread = threading.Thread(target=print_stdout)
+    # stdout_thread = threading.Thread(target=print_stdout)
+    stdout_thread = Consumer(queue, output)
     stdout_thread.start()
     time.sleep(10)
     if process.is_alive():
-        test_case.query(f"ALTER SESSION SET SCRIPT_OUTPUT_ADDRESS='{local_ip}:{port}';")
+        query(f"ALTER SESSION SET SCRIPT_OUTPUT_ADDRESS='{local_ip}:{port}';")
         return process, queue, stdout_thread
-    stdout_thread.keep_going = False
+    # stdout_thread.keep_going = False
+    stdout_thread.stop()
     queue.put("Cancel")  # Send message to cancel stdout_thread
-    test_case.fail("Could not start udf_debug.py")
+    # raise exception test_case.fail("Could not start udf_debug.py")
     return None, None, None
 
 
 class UdfDebugger:
-    """Context manager for temporary UDF output redirection."""
+    """
+    Context manager for temporary UDF output redirection.
+    """
 
     def __init__(
         self,
-        test_case: udf.TestCase,
-        server: Optional[str] = None,
-        output: Optional[io.TextIOBase] = sys.stdout,
+        query: QueryExecutor,
+        # test_case: udf.TestCase,
+        server: str | None = None,
+        output: TextStream | None = sys.stdout,
     ):
         self.output = output
-        self.test_case = test_case
+        self.query = query
         self.server = server
         self._process = None
         self._queue = None
@@ -152,13 +199,15 @@ class UdfDebugger:
         return self._activate()
 
     def start(self):
-        """Enter the debugger context explicitly."""
+        """
+        Enter the debugger context explicitly.
+        """
         return self._activate()
 
     def _activate(self):
         self._process, self._queue, self._stdout_thread = (
             start_udf_output_redirect_consumer(
-                test_case=self.test_case, server=self.server, output=self.output
+                query=self.query, server=self.server, output=self.output
             )
         )
         return self
@@ -166,27 +215,32 @@ class UdfDebugger:
     def __exit__(self, type_, value, trace_back):
         if self._process is not None:
             self._process.terminate()
-            # Wait 1s to give socket time to process all remaining messages
-            self._stdout_thread.keep_going = False
+            # Wait 1 second to give socket time to process all remaining messages.
+            #
+            # self._stdout_thread.keep_going = False
+            self._stdout_thread.stop()
             self._queue.put("Completed")
 
         self._process = None
         self._queue = None
 
     def stop(self, type_=None, value=None, trace_back=None):
-        """Exit the debugger context explicitly."""
+        """
+        Exit the debugger context explicitly.
+        """
         return self.__exit__(type_, value, trace_back)
 
 
-class UdfDebuggerFromDockerHost(UdfDebugger):
-    """UdfDebugger configured with the Docker host IP."""
-
-    def __init__(
-        self, test_case: udf.TestCase, output: Optional[io.TextIOBase] = sys.stdout
-    ):
-        env = docker_db_environment.DockerDBEnvironment("")
-        super().__init__(
-            test_case=test_case,
-            server=env.get_ip_address_of_host(),
-            output=output,
-        )
+# class UdfDebuggerFromDockerHost(UdfDebugger):
+#     """UdfDebugger configured with the Docker host IP."""
+#
+#     def __init__(
+#         # self, test_case: udf.TestCase, output: Optional[io.TextIOBase] = sys.stdout
+#         self, test_case: QueryExecutor, output: TextStream | None = sys.stdout
+#     ):
+#         env = docker_db_environment.DockerDBEnvironment("")
+#         super().__init__(
+#             test_case=test_case,
+#             server=env.get_ip_address_of_host(),
+#             output=output,
+#         )
