23452a41a311c174b4e2f2682333d4f11243977d
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-07-02 16:50:27 +0200

    Added test to verify script_languages is reset when leaving scope
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- test/integration/script_languages_test.py
+++ test/integration/script_languages_test.py
@@ -47,3 +47,38 @@ def test_setting_script_languages_wrapper(pytester):
     # testing against SaaS API is skipped
     assert result.ret == pytest.ExitCode.OK
     result.assert_outcomes(passed=1, skipped=1)
+
+
+def test_script_languages_is_reset_wrapper(pytester):
+    expected_script_languages = r"PYTHON3=PYTEST_SCL_INTEGRATION_TEST"
+    pytester.makepyfile(f"""
+    import pytest
+    from exasol.python_extension_common.deployment.language_container_deployer import (
+        LanguageActivationLevel,
+        get_language_settings,
+    )
+
+    def test_script_languages_is_reset(
+        activate_script_languages_for_function,
+        pyexasol_connection,
+    ):
+        current_script_languages = get_language_settings(
+            pyexasol_connection, LanguageActivationLevel.Session
+        )
+        assert current_script_languages == "{expected_script_languages}"
+
+    def test_script_languages_was_reset(pyexasol_connection):
+        current_script_languages = get_language_settings(
+            pyexasol_connection, LanguageActivationLevel.Session
+        )
+        assert current_script_languages != "{expected_script_languages}"
+    """)
+    result = pytester.runpytest(
+        BACKEND_OPTION,
+        BACKEND_ONPREM,
+        SCRIPT_LANGUAGES_OPTION,
+        expected_script_languages,
+    )
+    # testing against SaaS API is skipped
+    assert result.ret == pytest.ExitCode.OK
+    result.assert_outcomes(passed=2, skipped=2)
