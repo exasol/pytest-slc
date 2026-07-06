77397512d484023e579f688f6a2bbfbb8ae60d35
ckunki <christoph.kuhnke@exasol.com>
2026-07-06 09:08:53 +0200

    Added commends on CLI option --project-short-tag in itests

--- test/integration/pytest_slc_onprem_test.py
+++ test/integration/pytest_slc_onprem_test.py
@@ -9,6 +9,9 @@ from exasol.pytest_backend import (
 
 def test_pytest_slc_onprem(pytester):
     pytester.makepyfile(load_test_code())
+    # Technically, CLI option --project-short-tag is only required as
+    # distinctive prefix for SaaS instances.  See
+    # test/integration/pytest_slc_saas_test.py.
     result = pytester.runpytest(
         BACKEND_OPTION, BACKEND_ONPREM, "--project-short-tag", "PYSLC"
     )
--- test/integration/pytest_slc_saas_test.py
+++ test/integration/pytest_slc_saas_test.py
@@ -9,6 +9,10 @@ from exasol.pytest_backend import (
 
 def test_pytest_slc_saas(pytester):
     pytester.makepyfile(load_test_code())
+    # CLI option --project-short-tag is required as distinctive prefix for
+    # SaaS instances.  In case of a left-over database instance the prefix
+    # supports further analysis by indicating the project scope in which the
+    # database instance was created.
     result = pytester.runpytest(
         BACKEND_OPTION, BACKEND_SAAS, "--project-short-tag", "PYSLC"
     )
