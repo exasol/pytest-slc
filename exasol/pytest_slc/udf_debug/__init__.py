902cbae852826c0f573f0482a07ecaf15218cfca
ckunki <christoph.kuhnke@exasol.com>
2026-07-22 10:29:50 +0200

    Fixed imports

--- exasol/pytest_slc/udf_debug/__init__.py
+++ exasol/pytest_slc/udf_debug/__init__.py
@@ -1,14 +1,14 @@
 from exasol.pytest_slc.udf_debug.ip_address import IpAddress
-from exasol.pytest_slc.udf_debug.util import (
-    LogPipe,
-    pyexasol_query_func,
-)
 from exasol.pytest_slc.udf_debug.messages import wait_for_messages
 from exasol.pytest_slc.udf_debug.types import QueryResult
 from exasol.pytest_slc.udf_debug.udf_output_logger import (
     UdfOutputLogger,
     alter_session_sql,
     retrieve_script_output_address,
+)
+from exasol.pytest_slc.udf_debug.util import (
+    LogPipe,
+    pyexasol_query_func,
 )
 
 __all__ = [
@@ -17,6 +17,7 @@ __all__ = [
     "QueryResult",
     "UdfOutputLogger",
     "alter_session_sql",
+    "pyexasol_query_func",
     "retrieve_script_output_address",
     "wait_for_messages",
 ]
